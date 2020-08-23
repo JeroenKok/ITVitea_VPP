783dcd6b1d8d4033b1bc855a264d6c2bea19de82
JeroenKok <JeroenDavidKok@gmail.com>
2020-08-23 19:16:01 +0200

    Got most buttons to work.

--- Rood_Rekenmachine/Form1.Designer.cs
+++ Rood_Rekenmachine/Form1.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox = new System.Windows.Forms.TextBox();
+            this.textBoxFirstNumber = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -50,15 +50,18 @@
             this.buttonDel = new System.Windows.Forms.Button();
             this.buttonNegative = new System.Windows.Forms.Button();
             this.textBoxAnswer = new System.Windows.Forms.TextBox();
+            this.textBoxSecondNumber = new System.Windows.Forms.TextBox();
+            this.textBoxOperator = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // textBox
+            // textBoxFirstNumber
             // 
-            this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox.Location = new System.Drawing.Point(45, 47);
-            this.textBox.Name = "textBox";
-            this.textBox.Size = new System.Drawing.Size(370, 38);
-            this.textBox.TabIndex = 0;
+            this.textBoxFirstNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxFirstNumber.Location = new System.Drawing.Point(43, 23);
+            this.textBoxFirstNumber.Name = "textBoxFirstNumber";
+            this.textBoxFirstNumber.Size = new System.Drawing.Size(238, 31);
+            this.textBoxFirstNumber.TabIndex = 0;
+            this.textBoxFirstNumber.Text = "testLine";
             // 
             // button1
             // 
@@ -69,7 +72,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.buttonNumeric_Click);
+            this.button1.Click += new System.EventHandler(this.buttonGeneric_Click);
             // 
             // button2
             // 
@@ -80,7 +83,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "2";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.buttonNumeric_Click);
+            this.button2.Click += new System.EventHandler(this.buttonGeneric_Click);
             // 
             // button3
             // 
@@ -91,7 +94,7 @@
             this.button3.TabIndex = 3;
             this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.buttonNumeric_Click);
+            this.button3.Click += new System.EventHandler(this.buttonGeneric_Click);
             // 
             // button4
             // 
@@ -102,7 +105,7 @@
             this.button4.TabIndex = 1;
             this.button4.Text = "4";
             this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.buttonNumeric_Click);
+            this.button4.Click += new System.EventHandler(this.buttonGeneric_Click);
             // 
             // button5
             // 
@@ -113,7 +116,7 @@
             this.button5.TabIndex = 2;
             this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.buttonNumeric_Click);
+            this.button5.Click += new System.EventHandler(this.buttonGeneric_Click);
             // 
             // button6
             // 
@@ -124,7 +127,7 @@
             this.button6.TabIndex = 3;
             this.button6.Text = "6";
             this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.buttonNumeric_Click);
+            this.button6.Click += new System.EventHandler(this.buttonGeneric_Click);
             // 
             // button7
             // 
@@ -135,7 +138,7 @@
             this.button7.TabIndex = 1;
             this.button7.Text = "7";
             this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.buttonNumeric_Click);
+            this.button7.Click += new System.EventHandler(this.buttonGeneric_Click);
             // 
             // button8
             // 
@@ -146,7 +149,7 @@
             this.button8.TabIndex = 2;
             this.button8.Text = "8";
             this.button8.UseVisualStyleBackColor = true;
-            this.button8.Click += new System.EventHandler(this.buttonNumeric_Click);
+            this.button8.Click += new System.EventHandler(this.buttonGeneric_Click);
             // 
             // button9
             // 
@@ -157,7 +160,7 @@
             this.button9.TabIndex = 3;
             this.button9.Text = "9";
             this.button9.UseVisualStyleBackColor = true;
-            this.button9.Click += new System.EventHandler(this.buttonNumeric_Click);
+            this.button9.Click += new System.EventHandler(this.buttonGeneric_Click);
             // 
             // buttonDiv
             // 
@@ -168,6 +171,7 @@
             this.buttonDiv.TabIndex = 4;
             this.buttonDiv.Text = "/";
             this.buttonDiv.UseVisualStyleBackColor = true;
+            this.buttonDiv.Click += new System.EventHandler(this.buttonGeneric_Click);
             // 
             // buttonAdd
             // 
@@ -178,6 +182,7 @@
             this.buttonAdd.TabIndex = 5;
             this.buttonAdd.Text = "+";
             this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonGeneric_Click);
             // 
             // buttonCalc
             // 
@@ -188,6 +193,7 @@
             this.buttonCalc.TabIndex = 8;
             this.buttonCalc.Text = "=";
             this.buttonCalc.UseVisualStyleBackColor = true;
+            this.buttonCalc.Click += new System.EventHandler(this.buttonGeneric_Click);
             // 
             // button0
             // 
@@ -198,7 +204,7 @@
             this.button0.TabIndex = 10;
             this.button0.Text = "0";
             this.button0.UseVisualStyleBackColor = true;
-            this.button0.Click += new System.EventHandler(this.buttonNumeric_Click);
+            this.button0.Click += new System.EventHandler(this.buttonGeneric_Click);
             // 
             // buttonDecimalPoint
             // 
@@ -209,6 +215,7 @@
             this.buttonDecimalPoint.TabIndex = 10;
             this.buttonDecimalPoint.Text = ".";
             this.buttonDecimalPoint.UseVisualStyleBackColor = true;
+            this.buttonDecimalPoint.Click += new System.EventHandler(this.buttonGeneric_Click);
             // 
             // buttonAns
             // 
@@ -219,6 +226,7 @@
             this.buttonAns.TabIndex = 11;
             this.buttonAns.Text = "Ans";
             this.buttonAns.UseVisualStyleBackColor = true;
+            this.buttonAns.Click += new System.EventHandler(this.buttonGeneric_Click);
             // 
             // buttonSub
             // 
@@ -229,6 +237,7 @@
             this.buttonSub.TabIndex = 7;
             this.buttonSub.Text = "-";
             this.buttonSub.UseVisualStyleBackColor = true;
+            this.buttonSub.Click += new System.EventHandler(this.buttonGeneric_Click);
             // 
             // buttonMult
             // 
@@ -239,6 +248,7 @@
             this.buttonMult.TabIndex = 6;
             this.buttonMult.Text = "*";
             this.buttonMult.UseVisualStyleBackColor = true;
+            this.buttonMult.Click += new System.EventHandler(this.buttonGeneric_Click);
             // 
             // buttonClear
             // 
@@ -249,6 +259,7 @@
             this.buttonClear.TabIndex = 6;
             this.buttonClear.Text = "Clear";
             this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonGeneric_Click);
             // 
             // buttonDel
             // 
@@ -259,6 +270,7 @@
             this.buttonDel.TabIndex = 12;
             this.buttonDel.Text = "Del";
             this.buttonDel.UseVisualStyleBackColor = true;
+            this.buttonDel.Click += new System.EventHandler(this.buttonGeneric_Click);
             // 
             // buttonNegative
             // 
@@ -269,20 +281,44 @@
             this.buttonNegative.TabIndex = 7;
             this.buttonNegative.Text = "(-)";
             this.buttonNegative.UseVisualStyleBackColor = true;
+            this.buttonNegative.Click += new System.EventHandler(this.buttonGeneric_Click);
             // 
             // textBoxAnswer
             // 
-            this.textBoxAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxAnswer.Location = new System.Drawing.Point(294, 91);
+            this.textBoxAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxAnswer.Location = new System.Drawing.Point(43, 97);
             this.textBoxAnswer.Name = "textBoxAnswer";
-            this.textBoxAnswer.Size = new System.Drawing.Size(121, 38);
+            this.textBoxAnswer.ReadOnly = true;
+            this.textBoxAnswer.Size = new System.Drawing.Size(121, 31);
             this.textBoxAnswer.TabIndex = 13;
+            this.textBoxAnswer.Text = "testLine";
+            // 
+            // textBoxSecondNumber
+            // 
+            this.textBoxSecondNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSecondNumber.Location = new System.Drawing.Point(43, 60);
+            this.textBoxSecondNumber.Name = "textBoxSecondNumber";
+            this.textBoxSecondNumber.Size = new System.Drawing.Size(238, 31);
+            this.textBoxSecondNumber.TabIndex = 14;
+            this.textBoxSecondNumber.Text = "testLine";
+            // 
+            // textBoxOperator
+            // 
+            this.textBoxOperator.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxOperator.Location = new System.Drawing.Point(289, 60);
+            this.textBoxOperator.Name = "textBoxOperator";
+            this.textBoxOperator.ReadOnly = true;
+            this.textBoxOperator.Size = new System.Drawing.Size(43, 31);
+            this.textBoxOperator.TabIndex = 14;
+            this.textBoxOperator.Text = "testLine";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(457, 450);
+            this.Controls.Add(this.textBoxOperator);
+            this.Controls.Add(this.textBoxSecondNumber);
             this.Controls.Add(this.textBoxAnswer);
             this.Controls.Add(this.buttonDel);
             this.Controls.Add(this.buttonAns);
@@ -304,7 +340,7 @@
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.textBoxFirstNumber);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -314,7 +350,7 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.TextBox textBoxFirstNumber;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
@@ -336,6 +372,8 @@
         private System.Windows.Forms.Button buttonDel;
         private System.Windows.Forms.Button buttonNegative;
         private System.Windows.Forms.TextBox textBoxAnswer;
+        private System.Windows.Forms.TextBox textBoxSecondNumber;
+        private System.Windows.Forms.TextBox textBoxOperator;
     }
 }
 
--- Rood_Rekenmachine/Form1.cs
+++ Rood_Rekenmachine/Form1.cs
@@ -21,7 +21,9 @@ namespace Rood_Rekenmachine
         enum enumState
         {
             getFirstNumber,
-            getSecondNumber
+            getFirstNumberDecimal,
+            getSecondNumber,
+            getSecondNumberDecimal
         }
 
         enum enumOperator
@@ -29,17 +31,20 @@ namespace Rood_Rekenmachine
             add,
             sub,
             multiply,
-            divide
+            divide,
+            none
         }
                   
 
         decimal lastAnswer = 0; // so it doesn't crash when using Ans before Calc
-        decimal firstNumber;
-        decimal secondNumber;
+        decimal firstNumber = 0;
+        decimal secondNumber = 0;
+        string strOperator = "";
 
-        enumOperator chosenOperator;
+        enumOperator chosenOperator = enumOperator.none;
+        enumState calcState = enumState.getFirstNumber;
 
-        private void buttonNumeric_Click(object sender, EventArgs e)
+        private void buttonGeneric_Click(object sender, EventArgs e)
         {
             // get the first number
             //    untill getting an operator
@@ -47,88 +52,219 @@ namespace Rood_Rekenmachine
             //    untill getting calc command
             // show result
 
+            decimal gottenNumber = -1;
+
 
             // would use switch, but switch doesn't like button as a variable
             // Numeric buttons
             if (sender == button0)
             {
-                textBox.Text = "0";
+                gottenNumber = 0;
             }
             else if(sender == button1)
             {
-                textBox.Text = "1";
+                gottenNumber = 1;
             }
             else if (sender == button2)
             {
-                textBox.Text = "2";
+                gottenNumber = 2;
             }
             else if (sender == button3)
             {
-                textBox.Text = "3";
+                gottenNumber = 3;
             }
             else if (sender == button4)
             {
-                textBox.Text = "4";
+                gottenNumber = 4;
             }
             else if (sender == button5)
             {
-                textBox.Text = "5";
+                gottenNumber = 5;
             }
             else if (sender == button6)
             {
-                textBox.Text = "6";
+                gottenNumber = 6;
             }
             else if (sender == button7)
             {
-                textBox.Text = "7";
+                gottenNumber = 7;
             }
             else if (sender == button8)
             {
-                textBox.Text = "8";
+                gottenNumber = 8;
             }
             else if (sender == button9)
             {
-                textBox.Text = "9";
+                gottenNumber = 9;
             }
 
             // Calculus buttons
             else if (sender == buttonAdd)
             {
-
+                chosenOperator = enumOperator.add;
+                calcState = enumState.getSecondNumber;
             }
             else if (sender == buttonSub)
             {
-
+                chosenOperator = enumOperator.sub;
+                calcState = enumState.getSecondNumber;
             }
             else if (sender == buttonMult)
             {
-
+                chosenOperator = enumOperator.multiply;
+                calcState = enumState.getSecondNumber;
             }
             else if (sender == buttonDiv)
             {
-
+                chosenOperator = enumOperator.divide;
+                calcState = enumState.getSecondNumber;
             }
 
             // Fuctional buttons
             else if (sender == buttonDecimalPoint)
             {
-
+                if( calcState == enumState.getFirstNumber)
+                {
+                    calcState = enumState.getFirstNumberDecimal;
+                } else if (calcState == enumState.getSecondNumber)
+                {
+                    calcState = enumState.getSecondNumberDecimal;
+                }
             }
             else if (sender == buttonAns)
             {
-
+                if (calcState == enumState.getFirstNumber)
+                {
+                    firstNumber = lastAnswer;
+                }
+                else if (calcState == enumState.getSecondNumber)
+                {
+                    secondNumber = lastAnswer;
+                }
             }
             else if (sender == buttonCalc)
             {
-
+                // sending this to another function cause it's gonna be a big one.
+                calculateSum();
             }
             else if (sender == buttonDel)
             {
-
+                if (calcState == enumState.getFirstNumber)
+                {
+                    firstNumber = 0;
+                }
+                else if (calcState == enumState.getFirstNumberDecimal)
+                {
+                    firstNumber = 0;
+                    calcState = enumState.getFirstNumber;
+                }
+                else if (calcState == enumState.getSecondNumber)
+                {
+                    secondNumber = 0;
+                }
+                else if (calcState == enumState.getSecondNumberDecimal)
+                {
+                    secondNumber = 0;
+                    calcState = enumState.getSecondNumber;
+                }
             }
             else if (sender == buttonClear)
             {
+                firstNumber = 0;
+                secondNumber = 0;
+                chosenOperator = enumOperator.none;
+                lastAnswer = 0;
+                calcState = enumState.getFirstNumber;
+            }
 
+
+            if(gottenNumber != -1)
+            {
+                if (calcState == enumState.getFirstNumber)
+                {
+                    firstNumber *= 10;
+                    firstNumber += gottenNumber;
+                } else if(calcState == enumState.getFirstNumberDecimal)
+                {
+                    // while loop where I figure out how many decimal places the number has
+                    //     gottenNumber*10**-iterations;
+                    decimal tempFirstNumber = firstNumber%1; // get the remainder of firstnumber
+                    int num_iterations = 1;
+                    while( tempFirstNumber != 0M)
+                    {
+                        tempFirstNumber *= 10M;
+                        tempFirstNumber %= 1;
+                        num_iterations += 1;
+                    }
+                    firstNumber += gottenNumber * (decimal)Math.Pow(10, -num_iterations);
+                }
+                else if (calcState == enumState.getSecondNumber)
+                {
+                    secondNumber *= 10;
+                    secondNumber += gottenNumber;
+                }
+                else if (calcState == enumState.getSecondNumberDecimal)
+                {
+                    // while loop where I figure out how many decimal places the number has
+                    //     gottenNumber*10**-iterations;
+                    decimal tempSecondNumber = secondNumber % 1; // get the remainder of firstnumber
+                    int num_iterations = 1;
+                    while (tempSecondNumber != 0M)
+                    {
+                        tempSecondNumber *= 10M;
+                        tempSecondNumber %= 1;
+                        num_iterations += 1;
+                    }
+                    secondNumber += gottenNumber * (decimal)Math.Pow(10, -num_iterations);
+                }
+            }
+
+            // Format the textfield with the sum
+
+            textBoxFirstNumber.Text = Convert.ToString(firstNumber);
+            textBoxSecondNumber.Text = Convert.ToString(secondNumber);
+
+            switch(chosenOperator)
+            {
+                case enumOperator.add:
+                    textBoxOperator.Text = "+";
+                    break;
+                case enumOperator.sub:
+                    textBoxOperator.Text = "-";
+                    break;
+                case enumOperator.multiply:
+                    textBoxOperator.Text = "*";
+                    break;
+                case enumOperator.divide:
+                    textBoxOperator.Text = "/";
+                    break;
+                case enumOperator.none:
+                    textBoxOperator.Text = "";
+                    break;
+            }
+
+            textBoxAnswer.Text = Convert.ToString(lastAnswer);
+        }
+
+        void calculateSum()
+        {
+            if(firstNumber != 0M && secondNumber != 0M && chosenOperator != enumOperator.none) {
+
+                switch (chosenOperator)
+                {
+                    case enumOperator.add:
+                        lastAnswer = firstNumber + secondNumber;
+                        break;
+                    case enumOperator.sub:
+                        lastAnswer = firstNumber - secondNumber;
+                        break;
+                    case enumOperator.multiply:
+                        lastAnswer = firstNumber * secondNumber;
+                        break;
+                    case enumOperator.divide:
+                        lastAnswer = firstNumber / secondNumber;
+                        break;
+                }
             }
         }
     }
