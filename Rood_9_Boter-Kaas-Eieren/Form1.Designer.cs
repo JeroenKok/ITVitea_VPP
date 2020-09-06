eb9178caf52fe1bd6ca7310a7ab65d38eb1a79ed
Jeroen Kok <JeroenDavidKok@gmail.com>
2020-09-06 15:42:14 +0200

    Major progress on game.
    Most things seem finished? Could use some better coding, but MVP.

---
+++ Rood_9_Boter-Kaas-Eieren/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Rood_9_Boter_Kaas_Eieren
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox0 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxTurn = new System.Windows.Forms.PictureBox();
+            this.labelTurn = new System.Windows.Forms.Label();
+            this.pictureBox_Background = new System.Windows.Forms.PictureBox();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.labelWinner = new System.Windows.Forms.Label();
+            this.pictureBoxWinner = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTurn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Background)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWinner)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox0
+            // 
+            this.pictureBox0.Location = new System.Drawing.Point(209, 96);
+            this.pictureBox0.Name = "pictureBox0";
+            this.pictureBox0.Size = new System.Drawing.Size(49, 50);
+            this.pictureBox0.TabIndex = 0;
+            this.pictureBox0.TabStop = false;
+            this.pictureBox0.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(264, 96);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(49, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(319, 96);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(49, 50);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(209, 152);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(49, 50);
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(264, 152);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(49, 50);
+            this.pictureBox4.TabIndex = 0;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(319, 152);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(49, 50);
+            this.pictureBox5.TabIndex = 0;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(209, 208);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(49, 50);
+            this.pictureBox6.TabIndex = 0;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(264, 208);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(49, 50);
+            this.pictureBox7.TabIndex = 0;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(319, 208);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(49, 50);
+            this.pictureBox8.TabIndex = 0;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBoxTurn
+            // 
+            this.pictureBoxTurn.Location = new System.Drawing.Point(55, 96);
+            this.pictureBoxTurn.Name = "pictureBoxTurn";
+            this.pictureBoxTurn.Size = new System.Drawing.Size(49, 50);
+            this.pictureBoxTurn.TabIndex = 1;
+            this.pictureBoxTurn.TabStop = false;
+            // 
+            // labelTurn
+            // 
+            this.labelTurn.AutoSize = true;
+            this.labelTurn.Location = new System.Drawing.Point(52, 80);
+            this.labelTurn.Name = "labelTurn";
+            this.labelTurn.Size = new System.Drawing.Size(61, 13);
+            this.labelTurn.TabIndex = 2;
+            this.labelTurn.Text = "Player Turn";
+            // 
+            // pictureBox_Background
+            // 
+            this.pictureBox_Background.Location = new System.Drawing.Point(202, 87);
+            this.pictureBox_Background.Name = "pictureBox_Background";
+            this.pictureBox_Background.Size = new System.Drawing.Size(177, 180);
+            this.pictureBox_Background.TabIndex = 3;
+            this.pictureBox_Background.TabStop = false;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(55, 304);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 23);
+            this.buttonReset.TabIndex = 4;
+            this.buttonReset.Text = "Reset Game";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // labelWinner
+            // 
+            this.labelWinner.AutoSize = true;
+            this.labelWinner.Location = new System.Drawing.Point(429, 96);
+            this.labelWinner.Name = "labelWinner";
+            this.labelWinner.Size = new System.Drawing.Size(41, 13);
+            this.labelWinner.TabIndex = 6;
+            this.labelWinner.Text = "Winner";
+            // 
+            // pictureBoxWinner
+            // 
+            this.pictureBoxWinner.Location = new System.Drawing.Point(432, 112);
+            this.pictureBoxWinner.Name = "pictureBoxWinner";
+            this.pictureBoxWinner.Size = new System.Drawing.Size(49, 50);
+            this.pictureBoxWinner.TabIndex = 5;
+            this.pictureBoxWinner.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelWinner);
+            this.Controls.Add(this.pictureBoxWinner);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.labelTurn);
+            this.Controls.Add(this.pictureBoxTurn);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox0);
+            this.Controls.Add(this.pictureBox_Background);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.pictureBox_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTurn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Background)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWinner)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox0;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBoxTurn;
+        private System.Windows.Forms.Label labelTurn;
+        private System.Windows.Forms.PictureBox pictureBox_Background;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Label labelWinner;
+        private System.Windows.Forms.PictureBox pictureBoxWinner;
+    }
+}
+
---
+++ Rood_9_Boter-Kaas-Eieren/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Rood_9_Boter_Kaas_Eieren
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        enum GameState
+        {
+            noWinner,
+            winner
+        }
+
+        enum CellState
+        {
+            empty,
+            X,
+            O
+        }
+
+        const int maxCells = 9;
+        const int HoriCells = 3;
+        const int VertCells = 3;
+
+        CellState[] cellStates = new CellState[maxCells];
+        System.Windows.Forms.PictureBox[] pictureBoxes = new System.Windows.Forms.PictureBox[maxCells];
+
+        CellState playerTurn;
+        GameState gameState;
+
+        Dictionary<CellState, Image> imageDictionary = new Dictionary<CellState, Image>();
+
+        Image background;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.Assert(HoriCells * VertCells == maxCells,
+                "vertical and horizontal size don't match amount of cells",
+                "Yes, I know doing it like this is error prone. Might fix later.");
+
+            pictureBoxes[0] = pictureBox0;
+            pictureBoxes[1] = pictureBox1;
+            pictureBoxes[2] = pictureBox2;
+            pictureBoxes[3] = pictureBox3;
+            pictureBoxes[4] = pictureBox4;
+            pictureBoxes[5] = pictureBox5;
+            pictureBoxes[6] = pictureBox6;
+            pictureBoxes[7] = pictureBox7;
+            pictureBoxes[8] = pictureBox8;
+
+            string pathMod = "./../..";
+
+            string imagePath = Path.Combine(Environment.CurrentDirectory, pathMod, "X.png");
+            imageDictionary.Add(CellState.X, Image.FromFile(imagePath));
+
+            imagePath = Path.Combine(Environment.CurrentDirectory, pathMod, "O.png");
+            imageDictionary.Add(CellState.O, Image.FromFile(imagePath));
+
+
+
+            imagePath = Path.Combine(Environment.CurrentDirectory, pathMod, "BKE_Background.png");
+            background = Image.FromFile(imagePath);
+
+            pictureBox_Background.Image = background;
+
+            reset_game();
+        }
+
+        private void reset_game()
+        {
+            for (int i = 0; i < maxCells; i++)
+            {
+                cellStates[i] = CellState.empty;
+                pictureBoxes[i].Image = null;
+            }
+            pictureBoxWinner.Image = null;
+
+            playerTurn = CellState.X;
+            gameState = GameState.noWinner;
+            pictureBoxTurn.Image = imageDictionary[CellState.X];
+        }
+
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+            if(gameState == GameState.winner)
+            {
+                return;
+            }
+
+            for(int i = 0; i < maxCells; i++)
+            {
+                if( sender == pictureBoxes[i])
+                {
+                    if(cellStates[i] == CellState.empty)
+                    {
+                        pictureBoxes[i].Image = imageDictionary[playerTurn];
+                        cellStates[i] = playerTurn;
+                    }
+                }
+            }
+            if( check_Winner() )
+            {
+                return;
+            }
+            changeTurn();
+        }
+
+        private void changeTurn()
+        {
+            if(playerTurn == CellState.X)
+            {
+                playerTurn = CellState.O;
+                pictureBoxTurn.Image = imageDictionary[playerTurn];
+            } else if(playerTurn == CellState.O)
+            {
+                playerTurn = CellState.X;
+                pictureBoxTurn.Image = imageDictionary[playerTurn];
+            }
+        }
+
+
+        private bool check_Winner()
+        {
+            // check hori cells (read verti)
+            CellState winner = CellState.empty;
+            if( cellStates[0] == cellStates[1] && cellStates[1] == cellStates[2] && cellStates[2] != CellState.empty)
+            {
+                winner = playerTurn;
+            } else
+            if (cellStates[3] == cellStates[4] && cellStates[4] == cellStates[5] && cellStates[5] != CellState.empty)
+            {
+                winner = playerTurn;
+            }
+            else if (cellStates[6] == cellStates[7] && cellStates[7] == cellStates[8] && cellStates[8] != CellState.empty)
+            {
+                winner = playerTurn;
+            }
+            else
+            //check veri cells (read hori)
+            if (cellStates[0] == cellStates[3] && cellStates[3] == cellStates[6] && cellStates[6] != CellState.empty)
+            {
+                winner = playerTurn;
+            }
+            else if (cellStates[1] == cellStates[4] && cellStates[4] == cellStates[7] && cellStates[7] != CellState.empty)
+            {
+                winner = playerTurn;
+            }
+            else if (cellStates[2] == cellStates[5] && cellStates[5] == cellStates[8] && cellStates[8] != CellState.empty)
+            {
+                winner = playerTurn;
+            }
+            else
+
+            //check diag cells
+            if (cellStates[0] == cellStates[4] && cellStates[4] == cellStates[8] && cellStates[8] != CellState.empty)
+            {
+                winner = playerTurn;
+            }
+            else if (cellStates[2] == cellStates[4] && cellStates[4] == cellStates[6] && cellStates[6] != CellState.empty)
+            {
+                winner = playerTurn;
+            }
+
+            if( winner != CellState.empty)
+            {
+                gameState = GameState.winner;
+                pictureBoxWinner.Image = imageDictionary[playerTurn];
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            reset_game();
+        }
+    }
+}
